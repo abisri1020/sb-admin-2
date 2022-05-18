3ee8afa894fffa1e7c78ceac93c975b62a068fdd
FIRST_NAME LAST_NAME <sriabiramiece@gmail.com>
2022-05-18 17:51:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Dashboard from "./Dashboard";
+import Sidebar from "./Sidebar";
+import Topbar from "./Topbar";
+import { Switch, Route } from "react-router-dom";
+import Products from "./Products";
+import Users from "./Users";
+import CreateUser from "./CreateUser";
+import Editusers from "./EditUser";
+import { UserProvider } from "./UserContext";
+import { ProductProvider } from "./ProductContext";
+import CreateProducts from "./CreateProducts";
+import Editproducts from "./Editproducts";
+import {Login} from "./login"
+import {Error} from "./404error"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App wrapper">
+      <Sidebar />
+      <div className="content-wrapper d-flex flex-column">
+        <div className="content">
+          <Topbar />
+          <Switch>
+            <Route path="/users" exact>
+              <UserProvider>
+                <Users />
+              </UserProvider>
+            </Route>
+
+            <Route path="/products" exact>
+              <ProductProvider>
+                <Products />
+              </ProductProvider>
+            </Route>
+
+            <Route exact path="/login">
+              <Login/> 
+       </Route>
+       <Route exact path="/404">
+              <Error/> 
+       </Route>
+
+            <Route path="/" component={Dashboard} exact />
+
+            <Route path="/createuser" exact>
+              <UserProvider>
+                <CreateUser />
+              </UserProvider>
+            </Route>
+
+            <Route path="/createproducts" exact>
+              <ProductProvider>
+                <CreateProducts />
+              </ProductProvider>
+            </Route>
+
+            <>
+              <UserProvider>
+                <Route path="/users/edit/:id" component={Editusers}></Route>
+              </UserProvider>
+
+              <ProductProvider>
+                <Route
+                  path="/products/edit/:id"
+                  component={Editproducts}
+                ></Route>
+              </ProductProvider>
+            </>
+          </Switch>
+        </div>
+      </div>
     </div>
   );
 }
